483a7b7c49fde6e65d35b8313aa0541d57fbfc8d
Anant Goel <anantdgoel@gmail.com>
2017-02-25 11:14:49 -0600

    init

---
+++ nlp/parts_of_speech.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "github.com/kamildrazkiewicz/go-stanford-nlp"
+  "fmt"
+)
+
+func main() {
+    var (
+        tagger *pos.Tagger
+        res    []*pos.Result
+        err    error
+    )
+
+    if tagger, err = pos.NewTagger(
+        "./ext/stanford-nlp/models/english-left3words-distsim.tagger",    // path to model
+        "./ext/stanford-nlp/stanford-postagger-3.7.0.jar"); err != nil { // path to jar tagger file
+        fmt.Print(err)
+        return
+    }
+    if res, err = tagger.Tag("What is your name?"); err != nil {
+        fmt.Print(err)
+        return
+    }
+    for _, r := range res {
+        fmt.Println(r.Word, r.TAG, r.Description())
+    }
+
+}
